deeffdfb36e8eeba01e1a3c75eeadb45607c25a9
Diogo Almeida <diogodma80@gmail.com>
2023-07-10 11:39:09 -0400

    Create DesignTacoController

--- src/test/java/com/dma/taco/entity/Ingredient.java
+++ src/test/java/com/dma/taco/entity/Ingredient.java
@@ -1,5 +1,15 @@
 package com.dma.taco.entity;
 
+import lombok.Data;
+
+@Data
 public class Ingredient {
 
+	private final String id;
+	private final String name;
+	private final Type type;
+	
+	public static enum Type {
+		WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
+	}
 }
