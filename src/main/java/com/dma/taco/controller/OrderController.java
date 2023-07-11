7f0c26199e74a0ebe5b2396c58e0e7eea9619541
Diogo Almeida <diogodma80@gmail.com>
2023-07-10 20:22:36 -0400

    Create the Order entity

--- src/main/java/com/dma/taco/controller/OrderController.java
+++ src/main/java/com/dma/taco/controller/OrderController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.dma.taco.entity.Order;
+
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
