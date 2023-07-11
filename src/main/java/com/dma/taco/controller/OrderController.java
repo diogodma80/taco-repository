9bc8cfcbb04f7aa48cabbc73fc2d578a83d2b03e
Diogo Almeida <diogodma80@gmail.com>
2023-07-10 20:47:38 -0400

    Add validation errors to views

--- src/main/java/com/dma/taco/controller/OrderController.java
+++ src/main/java/com/dma/taco/controller/OrderController.java
@@ -1,8 +1,12 @@
 package com.dma.taco.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.dma.taco.entity.Order;
@@ -19,6 +23,17 @@ public class OrderController {
 		model.addAttribute("order", new Order());
 		
 		return "orderForm";
+	}
+	
+	@PostMapping
+	public String processOrder(@Valid Order order, Errors errors) {
+		if(errors.hasErrors()) {
+			return "orderForm";
+		}
+		
+		log.info("Order submitted " + order);
+		
+		return "redirect:/";
 	}
 	
 }
